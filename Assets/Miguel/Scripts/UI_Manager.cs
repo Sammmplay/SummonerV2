bfab22062699f679567a108b6490b7ae401e1eec
Guacamole1234 <Thegamerlapalma@gmail.com>
2025-05-01 12:52:34 +0100

    01/05/25
    Improves.exe.2

--- Assets/Miguel/Scripts/UI_Manager.cs
+++ Assets/Miguel/Scripts/UI_Manager.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
-    [SerializeField] GameObject mainMenu, title;
+    [SerializeField] GameObject mainMenu, title, menuEND, menuPause;
     
     private void Start()
     {
-        LeanTween.moveLocal(mainMenu, new Vector3(0f, -25f, 0f), .85f).setDelay(5f).setEase(LeanTweenType.easeOutCubic);
+        mainMenu.SetActive(true);
+        menuEND.SetActive(false);
+        menuPause.SetActive(false);
+
+        LeanTween.moveLocal(mainMenu, new Vector3(0f, -25f, 0f), .85f).setDelay(4f).setEase(LeanTweenType.easeOutCubic);
         LeanTween.scale(title, new Vector3(0f, 0f, 0f), .01f).setEase(LeanTweenType.easeInOutSine);
         LeanTween.scale(title, new Vector3(.6f, .6f, .6f), 3f).setDelay(.2f).setEase(LeanTweenType.easeInOutCubic);
         LeanTween.moveLocal(title, new Vector3(0f, 250f, 0f), 2.5f).setDelay(1.5f).setEase(LeanTweenType.easeInOutCubic);
+    }
 
+    private void menuFinal()
+    {
+        menuEND.SetActive(true);
+        LeanTween.moveLocal(menuEND, new Vector3(0f, 0f, 0f), .75f).setDelay(.2f).setEase(LeanTweenType.easeOutElastic);
+    }
+
+    private void menuStop()
+    {
+        menuPause.SetActive(true);
+        LeanTween.moveLocal(menuPause, new Vector3(0f, 0f, 0f), .75f).setDelay(.2f).setEase(LeanTweenType.easeOutElastic);
     }
 }
