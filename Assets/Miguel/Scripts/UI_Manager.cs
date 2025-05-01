d3350fbc163f7b424cc79e863a62f8b899ea305e
Guacamole1234 <Thegamerlapalma@gmail.com>
2025-05-01 11:29:40 +0100

    01/05/25
    Improves.exe

--- Assets/Miguel/Scripts/UI_Manager.cs
+++ Assets/Miguel/Scripts/UI_Manager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class UI_Manager : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu, title;
-
+    
     private void Start()
     {
-        LeanTween.moveLocal(mainMenu, new Vector3(450f, 0f, 0f), .5f).setDelay(5f).setEase(LeanTweenType.easeOutCubic);
-        LeanTween.scale(title, new Vector3(4f, 4f, 4f), 2f).setEase(LeanTweenType.easeInCubic);
-        LeanTween.moveLocal(title, new Vector3(0f, 330f, 0f), 2.7f).setDelay(2f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.moveLocal(mainMenu, new Vector3(0f, -25f, 0f), .85f).setDelay(5f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.scale(title, new Vector3(0f, 0f, 0f), .01f).setEase(LeanTweenType.easeInOutSine);
+        LeanTween.scale(title, new Vector3(.6f, .6f, .6f), 3f).setDelay(.2f).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.moveLocal(title, new Vector3(0f, 250f, 0f), 2.5f).setDelay(1.5f).setEase(LeanTweenType.easeInOutCubic);
+
     }
 }
